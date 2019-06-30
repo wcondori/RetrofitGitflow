b6b435dabac79a4f57823a0e190f5dd458927b70
Washington Edgar Condori <wcondori2015@gmail.com>
2019-06-30 12:08:59 -0500

    version 1

--- app/src/main/java/pe/edu/cibertec/retrofitgitflow/MainActivity.java
+++ app/src/main/java/pe/edu/cibertec/retrofitgitflow/MainActivity.java
@@ -3,12 +3,56 @@ package pe.edu.cibertec.retrofitgitflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textViewResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewResult = findViewById(R.id.textviewresul);
+        callService();
     }
+
+    private void callService() {
+       Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("");
+                }
+                else{
+                     List<Post> posts = response.body();
+                    textViewResult.setText(posts.toString());
+
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+
+            }
+        });
+
+
+    }
+
 }
---
+++ app/src/main/java/pe/edu/cibertec/retrofitgitflow/Post.java
@@ -0,0 +1,53 @@
+package pe.edu.cibertec.retrofitgitflow;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    private  int userId;
+    private  int id;
+    private  String title;
+    @SerializedName("body")
+    private  String text;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
